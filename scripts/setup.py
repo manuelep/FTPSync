0a4da404353ba23d50b5c48f7031ddb1b0b1d45a
manuele pesenti <manuele@valis-e.com>
2015-12-29 09:57:00 +0100

    debug: bigger timeout

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
+db.scheduler_task.truncate('RESTART IDENTITY CASCADE')
+
 scheduler.queue_task("fetchall",
-    timeout = 180,               #seconds
+    timeout = 600,               #seconds
     prevent_drift = True,
     period = 900,                #seconds
     immediate = False,
     repeats = 0,                 # 0=unlimited
     retry_failed = -1,           # -1=unlimited
 )
+
+db.commit()
