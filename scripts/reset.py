2acb69525f96ac9e4099e549c1a28e6370881f46
manuele pesenti <manuele@valis-e.com>
2016-01-27 23:18:43 +0100

    debug: introduced reset script

---
+++ scripts/reset.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+def _clean():
+    db.archive.truncate("CASCADE")
+    for path in (archive_upload_path, appconf.misc.dest_path):        
+        try:
+            shutil.rmtree(path)
+        except:
+            pass
+
+if __name__ == "__main__":
+    _clean()
